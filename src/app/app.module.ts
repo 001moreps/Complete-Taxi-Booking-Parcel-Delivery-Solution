d365a4b57e3c946cf60a3146df8612118263fe66
001moreps <001moreps@gmail.com>
2026-09-21 16:03:25 +0000

    feat(firebase): wire AngularFire into customer app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
  declarations:[AppComponent],
- imports:[BrowserModule,IonicModule.forRoot(),AppRoutingModule,FormsModule,ReactiveFormsModule],
+ imports:[BrowserModule,IonicModule.forRoot(),AngularFireModule.initializeApp(environment.firebase),AngularFirestoreModule,AngularFireFunctionsModule,AppRoutingModule,FormsModule,ReactiveFormsModule],
  providers:[{provide:RouteReuseStrategy,useClass:IonicRouteStrategy}],
  bootstrap:[AppComponent]
 })
